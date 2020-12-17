74b60bd5360776763bc3028e7521b01367a794f1
andrei valuika <andrei_valuika@git.epam.com>
2020-12-17 08:51:08 +0300

    Fix UI

--- Browser.cs
+++ Browser.cs
@@ -10,28 +10,17 @@ namespace Epam.TrainingTask.AutoTest
 	{
 		private static Browser _currentInstane;
 		private static IWebDriver _driver;
-		//public static BrowserFactory.BrowserType CurrentBrowser;
 		public static int ImplWait;
 		public static double TimeoutForElement;
-		private static string _browser;
 
 		private Browser()
 		{
-			InitParamas();
 			ChromeOptions options = new ChromeOptions();
 			options.AddArgument("disable-infobars");
 			_driver = new ChromeDriver(options);
-
-			//	_driver = BrowserFactory.GetDriver(CurrentBrowser, ImplWait);
 		}
 
-		private static void InitParamas()
-		{
-	//		ImplWait = Convert.ToInt32(Configuration.ElementTimeout);
-		//	TimeoutForElement = Convert.ToDouble(Configuration.ElementTimeout);
-		//	_browser = Configuration.Browser;
-		//	Enum.TryParse(_browser, out CurrentBrowser);
-		}
+		
 
 		public static Browser Instance => _currentInstane ?? (_currentInstane = new Browser());
 
@@ -55,7 +44,6 @@ namespace Epam.TrainingTask.AutoTest
 			_driver.Quit();
 			_currentInstane = null;
 			_driver = null;
-			_browser = null;
 		}
 	}
 }
--- GitAccountTest.cs
+++ GitAccountTest.cs
@@ -13,131 +13,87 @@ namespace Epam.TrainingTask.AutoTest
     public class GitAccountTest
     {
         static Browser Browser = Browser.Instance;
+        private const string LOGIN = "valujkoTest";
+        private const string PASSWORD = "Still1717";
         private IWebDriver _driver;
         private const string SOURSE_URL = "https://github.com";
-        private By createButton = By.XPath("//*[@id='new_repository']/div[4]/button");
         private By menuButton = By.XPath("//header/div[7]/details/summary");
-        private By deleteRepoButton = By.XPath("//ul/li[4]/details/summary");
-        private By changeVisibleButton = By.XPath("//ul/li[1]/details/summary");
 
         [OneTimeSetUp]
-        public void Setup()
+        public void OneTimeSetup()
         {
             Browser = Browser.Instance;
-            //ChromeOptions options = new ChromeOptions();
-            //options.AddArgument("disable-infobars");
-            //_driver = new ChromeDriver(options);
             _driver = Browser.GetDriver();
-            //_driver.Navigate().GoToUrl(SOURSE_URL);
-            new StartPage(_driver, SOURSE_URL).goLogin().logIn("valujkoTest", "Still1717").Maximize();
-            //_driver.FindElement(By.XPath("//a[@href='/login']")).Click();
 
-            //var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(3));
-            //IWebElement element = wait.Until(ExpectedConditions.ElementExists(By.Id("login_field")));
+            new StartPage(_driver, SOURSE_URL)
+                .goLogin()
+                .logIn(LOGIN, PASSWORD)
+                .Maximize();
 
-            //_driver.FindElement(By.Id("login_field")).SendKeys("valujkoTest");
-            //_driver.FindElement(By.Id("password")).SendKeys("Still1717");
+        }
 
-            //_driver.FindElement(By.Name("commit")).Click();
-            //var page = new LoginPage(_driver);
-            //page.logIn("valujkoTest", "Still1717").Maximize();
-            //_driver.Manage().Window.Maximize();
+        [SetUp]
+        public void Setup() 
+        {
+            Browser.NavigateTo(SOURSE_URL);
+        }
+
+        [TestCase("Tes21")]
+        public void CreateRepoTest(string repo)
+        {
+            var result = new MainPage(_driver)
+                .CreateRepo(repo)
+                .CheckRepoName(repo);
+
+            Assert.IsTrue(result);
         }
 
         [Test]
-        public void CreateRepoTest()
+        public void MakePrivateTest()
         {
-            _driver.FindElement(By.XPath("//*[@id='repos-container']/h2/a")).Click();
-            _driver.FindElement(By.Id("repository_name")).SendKeys("TestRepo111");
-
-            var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(3));
-            IWebElement element = wait.Until(ExpectedConditions.ElementToBeClickable(createButton));
-            _driver.FindElement(createButton).Click();
-            _driver.FindElement(By.LinkText("TestRepo111"));
-
-            //Assert.Pass();
-        }
-
-        [Test]
-        public void ChangeVisibleTest()
-        {
-
-            var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(3));
-            IWebElement element = wait.Until(ExpectedConditions.ElementToBeClickable(menuButton));
-            _driver.FindElement(menuButton).Click();
-
-            IWebElement element1 = wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//a[text()='Your repositories']")));
-            _driver.FindElement(By.XPath("//a[text()='Your repositories']")).Click();
-
-            IWebElement item = _driver.FindElement(By.XPath("//ul/li[1]/div[1]/div[1]/h3/a"));
-            string repoName = item.Text;
-            item.Click();
-
-            _driver.FindElement(By.XPath("//ul/li[9]/a")).Click();
-
-            IWebElement element3 = wait.Until(ExpectedConditions.ElementToBeClickable(changeVisibleButton));
-            _driver.FindElement(changeVisibleButton).Click();
-            Assert.Pass();
-        }
-        [Test]
-        public void DeleteRepoTest()
-        {
-            CreateRepoTest();
-
-
-            var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(3));
-            IWebElement element = wait.Until(ExpectedConditions.ElementToBeClickable(menuButton));
-            _driver.FindElement(menuButton).Click();
+            GoRepository();
+            new RepositoriesPage(_driver)
+                             .goSetting()
+                             .MakePrivate();
             
-            IWebElement element1 = wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//a[text()='Your repositories']")));
-            _driver.FindElement(By.XPath("//a[text()='Your repositories']")).Click();
-
-            IWebElement item = _driver.FindElement(By.XPath("//ul/li[1]/div[1]/div[1]/h3/a"));
-            string repoName = item.Text;
-            item.Click();
-            
-            _driver.FindElement(By.XPath("//ul/li[9]/a")).Click();
-
-            IWebElement element3 = wait.Until(ExpectedConditions.ElementToBeClickable(deleteRepoButton));
-            _driver.FindElement(deleteRepoButton).Click();
-
-            string delRepoName = _driver.FindElement(By.XPath("//*[@id='options_bucket']/div[10]/ul/li[4]/details/details-dialog/div[3]/p[2]/strong")).Text;
-            _driver.FindElement(By.XPath("//*[@id='options_bucket']/div[10]/ul/li[4]/details/details-dialog/div[3]/form/p/input")).SendKeys(delRepoName);
-
-            _driver.FindElement(By.XPath("//*[@id='options_bucket']/div[10]/ul/li[4]/details/details-dialog/div[3]/form/button")).Click();
             Assert.Pass();
         }
 
-        public void GoSetting()
+        [TestCase("Testi146542")]
+        public void DeleteRepoTest(string repo)
+        {
+            new MainPage(_driver).CreateRepo(repo);
+
+            GoRepository();
+            var result = new RepositoriesPage(_driver)
+                                .goSetting()
+                                .DeleteRepo()
+                                .CheckRepoName(repo);
+           
+            Assert.IsFalse(result);
+        }
+
+        public void GoRepository()
         {
             var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(3));
-            IWebElement element = wait.Until(ExpectedConditions.ElementToBeClickable(menuButton));
+            wait.Until(ExpectedConditions.ElementToBeClickable(menuButton));
             _driver.FindElement(menuButton).Click();
 
-            IWebElement element1 = wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//a[text()='Your repositories']")));
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//a[text()='Your repositories']")));
             _driver.FindElement(By.XPath("//a[text()='Your repositories']")).Click();
-
-            IWebElement item = _driver.FindElement(By.XPath("//ul/li[1]/div[1]/div[1]/h3/a"));
-            string repoName = item.Text;
-            item.Click();
-
-            _driver.FindElement(By.XPath("//ul/li[9]/a")).Click();
         }
 
 
-        [Test]
-        public void RenameRepoTest()
+        [TestCase("NewNa1")]
+        public void RenameRepoTest(string repo)
         {
-            GoSetting();
-            Thread.Sleep(1000);
-            _driver.FindElement(By.Name("new_name")).SendKeys(Keys.LeftControl + "a");
-            _driver.FindElement(By.Name("new_name")).SendKeys(Keys.Delete);
-            _driver.FindElement(By.Name("new_name")).SendKeys("New");
-            Thread.Sleep(100);
-            _driver.FindElement(By.XPath("//button[text()='Rename']")).Click();
-
-            var g =_driver.FindElement(By.XPath("//a[text()='New']"));
-            Assert.IsNotNull(g);
+            GoRepository();
+            var result = new RepositoriesPage(_driver)
+                             .goSetting()
+                             .Rename(repo)
+                             .getRepoName();
+                     
+            Assert.AreEqual(repo,result);
         }
 
        
